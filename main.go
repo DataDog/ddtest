7a3cef26c49a11dc0e4d52d2911740627dbd3b95
Andrey Marchenko <andrey.marchenko@datadoghq.com>
2025-07-10 14:12:55 +0200

    add Datadog APM

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/DataDog/dd-trace-go/v2/ddtrace/tracer"
+)
 
 func main() {
+	tracer.Start(tracer.WithService("datadog-test-runner"))
+	defer tracer.Stop()
+
+	span := tracer.StartSpan("greeting")
+	defer span.Finish()
+
+	span.SetTag("greeting", "Hello")
+
 	fmt.Println("Hello, World!")
 }
