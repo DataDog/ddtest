dc107b729fe82406f84c5726ba0fc49c2a7966ba
Andrey Marchenko <andrey.marchenko@datadoghq.com>
2025-08-07 13:46:10 +0200

    use slog, remove commented code

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log/slog"
 	"os"
 	"os/exec"
 	"time"
@@ -32,7 +33,7 @@ var testFilesCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		tags := make(map[string]string)
 		tags[constants.RuntimeName] = "ruby"
-		tags[constants.RuntimeVersion] = "3.3.3"
+		tags[constants.RuntimeVersion] = "3.3.0"
 		tags[constants.OSPlatform] = "darwin23"
 		tags[constants.OSVersion] = "24.5.0"
 		tags["language"] = "ruby"
@@ -47,7 +48,7 @@ var testFilesCmd = &cobra.Command{
 		ddSkippedTests := make(map[string]bool)
 
 		if librarySettings.ItrEnabled && librarySettings.TestsSkipping {
-			fmt.Println("Fetching skippable tests...")
+			slog.Debug("Fetching skippable tests...")
 			skippableTests := integrations.GetSkippableTests()
 
 			// fill the storage of all tests to be skipped
@@ -62,19 +63,22 @@ var testFilesCmd = &cobra.Command{
 		}
 		integrations.ExitCiVisibility()
 
-		fmt.Printf("Skipped tests: %d\n", len(ddSkippedTests))
-		// for testFQN := range ddSkippedTests {
-		// 	fmt.Println(testFQN)
-		// }
+		slog.Debug("Skipped tests", "count", len(ddSkippedTests))
 
 		durationTestOpt := time.Since(startTimeTestOpt)
-		fmt.Printf("Finished fetching skippable tests! (took %v)\n", durationTestOpt)
+		slog.Debug("Finished fetching skippable tests!", "duration", durationTestOpt)
 
 		filePath := "./.dd/tests-discovery/rspec.json"
+
+		// Delete the discovery file if it exists before running
+		if err := os.Remove(filePath); err != nil && !os.IsNotExist(err) {
+			slog.Warn("Warning: Failed to delete existing discovery file", "filePath", filePath, "error", err)
+		}
+
 		cmdName := "bundle"
 		cmdArgs := []string{"exec", "rspec", "--format", "progress", "--dry-run"}
 
-		fmt.Println("Starting RSpec dry run...")
+		slog.Debug("Starting RSpec dry run...")
 		startTime := time.Now()
 
 		rspecCmd := exec.Command(cmdName, cmdArgs...)
@@ -86,17 +90,17 @@ var testFilesCmd = &cobra.Command{
 		output, err := rspecCmd.CombinedOutput()
 
 		if err != nil {
-			fmt.Printf("Failed to run RSpec dry run with output: %s\n", output)
+			slog.Error("Failed to run RSpec dry run", "output", string(output))
 			os.Exit(1)
 		}
 
 		duration := time.Since(startTime)
-		fmt.Printf("Finished RSpec dry run! (took %v)\n", duration)
+		slog.Debug("Finished RSpec dry run!", "duration", duration)
 
 		// Read and parse the JSON stream file
 		file, err := os.Open(filePath)
 		if err != nil {
-			fmt.Printf("Error opening JSON file: %v\n", err)
+			slog.Error("Error opening JSON file", "error", err)
 			os.Exit(1)
 		}
 		defer file.Close()
@@ -106,17 +110,18 @@ var testFilesCmd = &cobra.Command{
 		for decoder.More() {
 			var test Test
 			if err := decoder.Decode(&test); err != nil {
-				fmt.Printf("Error parsing JSON: %v\n", err)
+				slog.Error("Error parsing JSON", "error", err)
 				os.Exit(1)
 			}
 			tests = append(tests, test)
 		}
 
-		fmt.Printf("Parsed RSpec report with %d examples\n", len(tests))
+		slog.Debug("Parsed RSpec report", "examples", len(tests))
 
 		testFiles := make(map[string]bool)
 		for _, test := range tests {
 			if !ddSkippedTests[test.FQN] {
+				slog.Debug("Test is not skipped", "test", test.FQN, "sourceFile", test.SourceFile)
 				testFiles[test.SourceFile] = true
 			}
 		}
@@ -132,52 +137,13 @@ func testFQN(suite, test, parameters string) string {
 	return fmt.Sprintf("%s.%s.%s", suite, test, parameters)
 }
 
-// func printSettings(settings net.SettingsResponseData) {
-// 	fmt.Printf("Library Settings:\n")
-// 	fmt.Printf("  ItrEnabled: %v\n", settings.ItrEnabled)
-// 	fmt.Printf("  TestsSkipping: %v\n", settings.TestsSkipping)
-// 	fmt.Printf("  CodeCoverage: %v\n", settings.CodeCoverage)
-// 	fmt.Printf("  RequireGit: %v\n", settings.RequireGit)
-// 	fmt.Printf("  FlakyTestRetriesEnabled: %v\n", settings.FlakyTestRetriesEnabled)
-// 	fmt.Printf("  KnownTestsEnabled: %v\n", settings.KnownTestsEnabled)
-// 	fmt.Printf("  ImpactedTestsEnabled: %v\n", settings.ImpactedTestsEnabled)
-// 	fmt.Printf("  EarlyFlakeDetection.Enabled: %v\n", settings.EarlyFlakeDetection.Enabled)
-// 	fmt.Printf("  EarlyFlakeDetection.FaultySessionThreshold: %d\n", settings.EarlyFlakeDetection.FaultySessionThreshold)
-// 	fmt.Printf("  TestManagement.Enabled: %v\n", settings.TestManagement.Enabled)
-// 	fmt.Printf("  TestManagement.AttemptToFixRetries: %d\n", settings.TestManagement.AttemptToFixRetries)
-// 	fmt.Println()
-// }
-
-// func printSkippableTests(skippableTests map[string]map[string][]net.SkippableResponseDataAttributes) {
-// 	fmt.Printf("Skippable Tests:\n")
-// 	fmt.Printf("  Total Suites: %d\n", len(skippableTests))
-
-// 	totalTests := 0
-// 	for suite, tests := range skippableTests {
-// 		totalTests += len(tests)
-// 		fmt.Printf("  Suite: %s (%d tests)\n", suite, len(tests))
-// 		for testName, testInstances := range tests {
-// 			fmt.Printf("    Test: %s (%d instances)\n", testName, len(testInstances))
-// 			for i, instance := range testInstances {
-// 				fmt.Printf("      Instance %d:\n", i+1)
-// 				fmt.Printf("        Parameters: %s\n", instance.Parameters)
-// 				fmt.Printf("        Configurations: OsPlatform=%s, OsArchitecture=%s, RuntimeName=%s, RuntimeVersion=%s\n",
-// 					instance.Configurations.OsPlatform, instance.Configurations.OsArchitecture,
-// 					instance.Configurations.RuntimeName, instance.Configurations.RuntimeVersion)
-// 			}
-// 		}
-// 	}
-// 	fmt.Printf("  Total Tests: %d\n", totalTests)
-// 	fmt.Println()
-// }
-
 func init() {
 	rootCmd.AddCommand(testFilesCmd)
 }
 
 func main() {
 	if err := rootCmd.Execute(); err != nil {
-		fmt.Println(err)
+		slog.Error("FAILURE", "error", err)
 		os.Exit(1)
 	}
 }
