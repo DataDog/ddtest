5c9ce1d09a93eb13bd9f24bc30365d6fffc5ec87
Andrey Marchenko <andrey.marchenko@datadoghq.com>
2025-08-26 17:28:18 +0200

    add os.architecure tag to the map of tags discovered from Ruby environment

--- internal/platform/scripts/ruby_env.rb
+++ internal/platform/scripts/ruby_env.rb
@@ -4,6 +4,7 @@ require "datadog/core/environment/platform"
 
 tags_map = {
   Datadog::CI::Ext::Test::TAG_OS_PLATFORM => ::RbConfig::CONFIG["host_os"],
+  Datadog::CI::Ext::Test::TAG_OS_ARCHITECTURE => ::RbConfig::CONFIG["host_cpu"],
   Datadog::CI::Ext::Test::TAG_OS_VERSION => Datadog::Core::Environment::Platform.kernel_release,
   Datadog::CI::Ext::Test::TAG_RUNTIME_NAME => Datadog::Core::Environment::Ext::LANG_ENGINE,
   Datadog::CI::Ext::Test::TAG_RUNTIME_VERSION => Datadog::Core::Environment::Ext::ENGINE_VERSION
