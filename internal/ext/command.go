ddfbe637b0c7805dc59b9d254498d765b758e7d8
Andrey Marchenko <andrey.marchenko@datadoghq.com>
2025-10-07 14:45:23 +0200

    add signal handling

--- internal/ext/command.go
+++ internal/ext/command.go
@@ -1,6 +1,10 @@
 package ext
 
-import "os/exec"
+import (
+	"os"
+	"os/exec"
+	"os/signal"
+)
 
 type CommandExecutor interface {
 	CombinedOutput(cmd *exec.Cmd) ([]byte, error)
@@ -14,5 +18,35 @@ func (e *DefaultCommandExecutor) CombinedOutput(cmd *exec.Cmd) ([]byte, error) {
 }
 
 func (e *DefaultCommandExecutor) Run(cmd *exec.Cmd) error {
-	return cmd.Run()
+	// Start the command
+	if err := cmd.Start(); err != nil {
+		return err
+	}
+
+	// Create a channel to signal when the command finishes
+	done := make(chan struct{})
+	defer close(done)
+
+	// Set up signal handling
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan)
+	defer signal.Stop(sigChan)
+
+	// Start a goroutine to forward signals to the process
+	go func() {
+		select {
+		case sig := <-sigChan:
+			// Forward the signal to the process
+			if cmd.Process != nil {
+				_ = cmd.Process.Signal(sig)
+			}
+		case <-done:
+			return
+		}
+	}()
+
+	// Wait for the command to finish
+	err := cmd.Wait()
+
+	return err
 }
