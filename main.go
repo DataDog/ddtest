e200e0f0c2bc670260d9f5b532c308b2c874c574
Andrey Marchenko <andrey.marchenko@datadoghq.com>
2025-10-09 14:31:59 +0200

    get rid of annoying appsec warning

--- main.go
+++ main.go
@@ -8,6 +8,10 @@ import (
 )
 
 func main() {
+	// this is weird, I know: I am trying to get rid of annoying appsec telemetry
+	// warning that is polluting the logs when Datadog Agent is not available
+	_ = os.Setenv("DD_TELEMETRY_DEPENDENCY_COLLECTION_ENABLED", "0")
+
 	if err := cmd.Execute(); err != nil {
 		slog.Error("FAILURE", "error", err)
 		os.Exit(1)
